a8fc829d9dca4b4e8d5a042cdfd5d318ab31bf96
Gabrielhva <148148776+Gabrielhva@users.noreply.github.com>
2024-09-05 17:15:22 -0300

    login linkando

---
+++ pages/login.vue
@@ -0,0 +1,95 @@
+<script>
+
+         function enviaFormulario(e){
+
+            e.preventDefault();  
+             
+            const usuario = {
+
+                nome:"",
+                senha:""
+
+
+            };
+
+            usuario.nome = e.target.nome.value;
+            usuario.senha = e.target.senha.value;
+
+            console.log("nome:",usuario.nome);
+            console.log("senha:",usuario.senha);
+
+        if (usuario.nome == "admin"){
+
+            console.log("Usuário correto");
+        }
+
+            else {
+
+
+            console.log("Usuário incorreto!");
+
+
+        }
+
+        if (usuario.senha == "senha"){
+
+            console.log("Senha correta");
+            
+        }
+
+            else {
+
+                console.log("Senha incorreta!");
+
+            }
+
+
+
+        
+            }
+</script>
+
+<template>
+
+<hr/>
+
+        <h2> Entrar </h2>
+        <br/><br/>
+
+        <p> Use sua conta para acessar seu painel de usuário </p>
+        <br/>
+
+        <p> Ainda nao tem uma conta? Cadastre-se agora mesmo. <a href="./cadastrar.html"> clicando aqui </a> </p>
+         <!-- depois validar -->
+         <br/>
+
+         <form action="./index.html">
+            <p> E-mail: </p>
+           <input/>
+
+            <br/>
+
+            <p> Senha: </p>
+            <input/>
+            <br/>
+            <br/>
+
+            <button> Entrar </button>
+         
+            <!-- dois validar button -->
+
+        </form>
+
+         
+
+        
+
+
+         
+
+    <hr/>
+
+
+
+    
+</template>

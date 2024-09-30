2c67cf7e8d0259bd6047a65a8b9c591361d8dd7a
Gabrielhva <148148776+Gabrielhva@users.noreply.github.com>
2024-09-30 17:08:08 -0300

    pagina de médicos

---
+++ pages/medlist.vue
@@ -0,0 +1,238 @@
+<script setup>
+
+import '~/assets/css/medlist.css'
+
+</script>
+
+
+<template>
+<section class="bg">
+<div class="container mx-auto mt-4 ">
+    
+  <div class="ajuste">
+    <section>
+    <div class="col-md-4">
+      <div class="card" style="width: 18rem;">
+  <img src="https://i.imgur.com/ZTkt4I5.jpg" class="img_perfil_med" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Gabriel Henrique</h5>
+        <h6 class="tipomed">Psiquiatra</h6>
+    <p class="card-text">Psiquiatra com 10 anos de experiência, Dr. Gabriel Henrique oferece atendimento empático e individualizado, especializado em psicofarmacologia e terapia cognitivo-comportamental.</p>
+       <NuxtLink to="#" class="btn mr-2"><i class="fa-solid fa-paper-plane"></i> Contatar</NuxtLink>
+       <NuxtLink to="#" class="btn"><i class="fa-solid fa-envelope"></i>Email</NuxtLink>
+  </div>
+  </div>
+  </div>    
+    </section>
+    
+    <div class="col-md-4">
+      <div class="card" style="width: 18rem;">
+  <img src="https://i.imgur.com/ZTkt4I5.jpg" class="img_perfil_med" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Gabriel Henrique</h5>
+        <h6 class="tipomed">Psiquiatra</h6>
+    <p class="card-text">Psiquiatra com 10 anos de experiência, Dr. Gabriel Henrique oferece atendimento empático e individualizado, especializado em psicofarmacologia e terapia cognitivo-comportamental.</p>
+       <NuxtLink to="#" class="btn mr-2"><i class="fa-solid fa-paper-plane"></i> Contatar</NuxtLink>
+       <NuxtLink to="#" class="btn"><i class="fa-solid fa-envelope"></i>Email</NuxtLink>
+  </div>
+  </div>
+  </div>   
+    
+  <div class="col-md-4">
+      <div class="card" style="width: 18rem;">
+  <img src="https://i.imgur.com/ZTkt4I5.jpg" class="img_perfil_med" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Gabriel Henrique</h5>
+        <h6 class="tipomed">Psiquiatra</h6>
+    <p class="card-text">Psiquiatra com 10 anos de experiência, Dr. Gabriel Henrique oferece atendimento empático e individualizado, especializado em psicofarmacologia e terapia cognitivo-comportamental.</p>
+       <NuxtLink to="#" class="btn mr-2"><i class="fa-solid fa-paper-plane"></i> Contatar</NuxtLink>
+       <NuxtLink to="#" class="btn"><i class="fa-solid fa-envelope"></i>Email</NuxtLink>
+  </div>
+  </div>
+  </div>   
+
+  <div class="col-md-4">
+      <div class="card" style="width: 18rem;">
+  <img src="https://i.imgur.com/ZTkt4I5.jpg" class="img_perfil_med" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Gabriel Henrique</h5>
+        <h6 class="tipomed">Psiquiatra</h6>
+    <p class="card-text">Psiquiatra com 10 anos de experiência, Dr. Gabriel Henrique oferece atendimento empático e individualizado, especializado em psicofarmacologia e terapia cognitivo-comportamental.</p>
+       <NuxtLink to="#" class="btn mr-2"><i class="fa-solid fa-paper-plane"></i> Contatar</NuxtLink>
+       <NuxtLink to="#" class="btn"><i class="fa-solid fa-envelope"></i>Email</NuxtLink>
+  </div>
+  </div>
+  </div>   
+
+  <div class="col-md-4">
+      <div class="card" style="width: 18rem;">
+  <img src="https://i.imgur.com/ZTkt4I5.jpg" class="img_perfil_med" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Gabriel Henrique</h5>
+        <h6 class="tipomed">Psiquiatra</h6>
+    <p class="card-text">Psiquiatra com 10 anos de experiência, Dr. Gabriel Henrique oferece atendimento empático e individualizado, especializado em psicofarmacologia e terapia cognitivo-comportamental.</p>
+       <NuxtLink to="#" class="btn mr-2"><i class="fa-solid fa-paper-plane"></i> Contatar</NuxtLink>
+       <NuxtLink to="#" class="btn"><i class="fa-solid fa-envelope"></i>Email</NuxtLink>
+  </div>
+  </div>
+  </div>   
+
+  
+      
+  </div>
+    </div>
+
+    <div class="container mx-auto mt-4">
+    
+    <div class="ajuste">
+      <section>
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+    <img src="https://i.imgur.com/ZTkt4I5.jpg" class="img_perfil_med" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Gabriel Henrique</h5>
+          <h6 class="tipomed">Psiquiatra</h6>
+      <p class="card-text">Psiquiatra com 10 anos de experiência, Dr. Gabriel Henrique oferece atendimento empático e individualizado, especializado em psicofarmacologia e terapia cognitivo-comportamental.</p>
+         <NuxtLink to="#" class="btn mr-2"><i class="fa-solid fa-paper-plane"></i> Contatar</NuxtLink>
+         <NuxtLink to="#" class="btn"><i class="fa-solid fa-envelope"></i>Email</NuxtLink>
+    </div>
+    </div>
+    </div>    
+      </section>
+      
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+    <img src="https://i.imgur.com/ZTkt4I5.jpg" class="img_perfil_med" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Gabriel Henrique</h5>
+          <h6 class="tipomed">Psiquiatra</h6>
+      <p class="card-text">Psiquiatra com 10 anos de experiência, Dr. Gabriel Henrique oferece atendimento empático e individualizado, especializado em psicofarmacologia e terapia cognitivo-comportamental.</p>
+         <NuxtLink to="#" class="btn mr-2"><i class="fa-solid fa-paper-plane"></i> Contatar</NuxtLink>
+         <NuxtLink to="#" class="btn"><i class="fa-solid fa-envelope"></i>Email</NuxtLink>
+    </div>
+    </div>
+    </div>   
+      
+    <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+    <img src="https://i.imgur.com/ZTkt4I5.jpg" class="img_perfil_med" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Gabriel Henrique</h5>
+          <h6 class="tipomed">Psiquiatra</h6>
+      <p class="card-text">Psiquiatra com 10 anos de experiência, Dr. Gabriel Henrique oferece atendimento empático e individualizado, especializado em psicofarmacologia e terapia cognitivo-comportamental.</p>
+         <NuxtLink to="#" class="btn mr-2"><i class="fa-solid fa-paper-plane"></i> Contatar</NuxtLink>
+         <NuxtLink to="#" class="btn"><i class="fa-solid fa-envelope"></i>Email</NuxtLink>
+    </div>
+    </div>
+    </div>   
+  
+    <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+    <img src="https://i.imgur.com/ZTkt4I5.jpg" class="img_perfil_med" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Gabriel Henrique</h5>
+          <h6 class="tipomed">Psiquiatra</h6>
+      <p class="card-text">Psiquiatra com 10 anos de experiência, Dr. Gabriel Henrique oferece atendimento empático e individualizado, especializado em psicofarmacologia e terapia cognitivo-comportamental.</p>
+         <NuxtLink to="#" class="btn mr-2"><i class="fa-solid fa-paper-plane"></i> Contatar</NuxtLink>
+         <NuxtLink to="#" class="btn"><i class="fa-solid fa-envelope"></i>Email</NuxtLink>
+    </div>
+    </div>
+    </div>   
+  
+    <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+    <img src="https://i.imgur.com/ZTkt4I5.jpg" class="img_perfil_med" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Gabriel Henrique</h5>
+          <h6 class="tipomed">Psiquiatra</h6>
+      <p class="card-text">Psiquiatra com 10 anos de experiência, Dr. Gabriel Henrique oferece atendimento empático e individualizado, especializado em psicofarmacologia e terapia cognitivo-comportamental.</p>
+         <NuxtLink to="#" class="btn mr-2"><i class="fa-solid fa-paper-plane"></i> Contatar</NuxtLink>
+         <NuxtLink to="#" class="btn"><i class="fa-solid fa-envelope"></i>Email</NuxtLink>
+    </div>
+    </div>
+    </div>   
+  
+    
+        
+    </div>
+    </div>
+
+    <div class="container mx-auto mt-4">
+    
+    <div class="ajuste">
+      <section>
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+    <img src="https://i.imgur.com/ZTkt4I5.jpg" class="img_perfil_med" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Gabriel Henrique</h5>
+          <h6 class="tipomed">Psiquiatra</h6>
+      <p class="card-text">Psiquiatra com 10 anos de experiência, Dr. Gabriel Henrique oferece atendimento empático e individualizado, especializado em psicofarmacologia e terapia cognitivo-comportamental.</p>
+         <NuxtLink to="#" class="btn mr-2"><i class="fa-solid fa-paper-plane"></i> Contatar</NuxtLink>
+         <NuxtLink to="#" class="btn"><i class="fa-solid fa-envelope"></i>Email</NuxtLink>
+    </div>
+    </div>
+    </div>    
+      </section>
+      
+      <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+    <img src="https://i.imgur.com/ZTkt4I5.jpg" class="img_perfil_med" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Gabriel Henrique</h5>
+          <h6 class="tipomed">Psiquiatra</h6>
+      <p class="card-text">Psiquiatra com 10 anos de experiência, Dr. Gabriel Henrique oferece atendimento empático e individualizado, especializado em psicofarmacologia e terapia cognitivo-comportamental.</p>
+         <NuxtLink to="#" class="btn mr-2"><i class="fa-solid fa-paper-plane"></i> Contatar</NuxtLink>
+         <NuxtLink to="#" class="btn"><i class="fa-solid fa-envelope"></i>Email</NuxtLink>
+    </div>
+    </div>
+    </div>   
+      
+    <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+    <img src="https://i.imgur.com/ZTkt4I5.jpg" class="img_perfil_med" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Gabriel Henrique</h5>
+          <h6 class="tipomed">Psiquiatra</h6>
+      <p class="card-text">Psiquiatra com 10 anos de experiência, Dr. Gabriel Henrique oferece atendimento empático e individualizado, especializado em psicofarmacologia e terapia cognitivo-comportamental.</p>
+         <NuxtLink to="#" class="btn mr-2"><i class="fa-solid fa-paper-plane"></i> Contatar</NuxtLink>
+         <NuxtLink to="#" class="btn"><i class="fa-solid fa-envelope"></i>Email</NuxtLink>
+    </div>
+    </div>
+    </div>   
+  
+    <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+    <img src="https://i.imgur.com/ZTkt4I5.jpg" class="img_perfil_med" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Gabriel Henrique</h5>
+          <h6 class="tipomed">Psiquiatra</h6>
+      <p class="card-text">Psiquiatra com 10 anos de experiência, Dr. Gabriel Henrique oferece atendimento empático e individualizado, especializado em psicofarmacologia e terapia cognitivo-comportamental.</p>
+         <NuxtLink to="#" class="btn mr-2"><i class="fa-solid fa-paper-plane"></i> Contatar</NuxtLink>
+         <NuxtLink to="#" class="btn"><i class="fa-solid fa-envelope"></i>Email</NuxtLink>
+    </div>
+    </div>
+    </div>   
+  
+    <div class="col-md-4">
+        <div class="card" style="width: 18rem;">
+    <img src="https://i.imgur.com/ZTkt4I5.jpg" class="img_perfil_med" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Gabriel Henrique</h5>
+          <h6 class="tipomed">Psiquiatra</h6>
+      <p class="card-text">Psiquiatra com 10 anos de experiência, Dr. Gabriel Henrique oferece atendimento empático e individualizado, especializado em psicofarmacologia e terapia cognitivo-comportamental.</p>
+         <NuxtLink to="#" class="btn mr-2"><i class="fa-solid fa-paper-plane"></i> Contatar</NuxtLink>
+         <NuxtLink to="#" class="btn"><i class="fa-solid fa-envelope"></i>Email</NuxtLink>
+    </div>
+    </div>
+    </div>   
+  
+    
+        
+    </div>
+    </div>
+</section>
+
+
+
+</template>

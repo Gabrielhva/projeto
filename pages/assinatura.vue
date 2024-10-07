d7844cbd8c1b0a8cbf9330c069f965d918e2f746
Gabrielhva <148148776+Gabrielhva@users.noreply.github.com>
2024-10-07 16:36:38 -0300

    css

--- pages/assinatura.vue
+++ pages/assinatura.vue
@@ -6,39 +6,44 @@ import '~/assets/css/assinatura.css'
 
 <template>
     
-<h1>Atenda muito mais com Premium</h1>
+<h1 class="tituloo">Atenda muito mais com Premium</h1>
 
-<section class="fundo">
+<section class="fundoo">
 
 
-<div class="container">
+<div class="containerr">
 
+    <div class="imga">
+        <img src="./imagens/black.jpg"/>
+
+        <img src="./imagens/black.jpg"/>
+    </div>
 
     <div class="texto-sobre-imagem">
 
-    <div class="texto texto2"><strong>Versão para Especialistas</strong></div>
-    <div class="texto texto3"><strong>Doenças Mentais</strong></div>
-    <div class="texto espaco"><strong>R$ 49,99 <p class="texto4">/Mês</p></strong></div>
-    <div class="texto texto5"><strong><span>✓</span> Destaque para os pacientes</strong></div>
-    <div class="texto texto6"><strong><span>✓</span> Perfil mais personalizado</strong></div>
-    <div class="texto texto7"><strong><span>✓</span> Maior taxa de resposta</strong></div>
+    <div class="textoo textoo2"><strong>Versão para Especialistas</strong></div>
+    <div class="textoo textoo3"><strong>Doenças Mentais</strong></div>
+    <div class="textoo espacoo"><strong>R$ 49,99 <p class="textoo4">/Mês</p></strong></div>
+    <div class="textoo textoo5"><strong><span class="spann">✓</span> Destaque para os pacientes</strong></div>
+    <div class="textoo textoo6"><strong><span class="spann">✓</span> Perfil mais personalizado</strong></div>
+    <div class="textoo textoo7"><strong><span class="spann">✓</span> Maior taxa de resposta</strong></div>
 
-    <div class="texto botao"><button>Assinar</button></div>
+    <div class="textoo botaoo"><button>Assinar</button></div>
 
 
     </div>
-    <div class="container2">
+    <div class="containerr2">
     <div class="texto-sobre-imagem">
 
 
-    <div class="texto texto2"><strong>Versão para Clínicas</strong></div>
-    <div class="texto texto3"><strong>Dependência Química</strong></div>
-    <div class="texto espaco"><strong>R$ 119,99 <p class="texto4">/Mês</p></strong></div>
-    <div class="texto texto5"><strong><span>✓</span> Destaque para os pacientes</strong></div>
-    <div class="texto texto6"><strong><span>✓</span> Layout de Clínica personalizado</strong></div>
-    <div class="texto texto7"><strong><span>✓</span> Maior taxa de resposta</strong></div>
+    <div class="textoo textoo2"><strong>Versão para Clínicas</strong></div>
+    <div class="textoo textoo3"><strong>Dependência Química</strong></div>
+    <div class="textoo espacoo"><strong>R$ 119,99 <p class="textoo4">/Mês</p></strong></div>
+    <div class="textoo textoo5"><strong><span class="spann">✓</span> Destaque para os pacientes</strong></div>
+    <div class="textoo textoo6"><strong><span class="spann">✓</span> Layout de Clínica personalizado</strong></div>
+    <div class="textoo textoo7"><strong><span class="spann">✓</span> Maior taxa de resposta</strong></div>
     
-    <div class="texto botao"><button>Assinar</button></div>
+    <div class="textoo botaoo"><button class="buttonn">Assinar</button></div>
     </div>
     </div>
 

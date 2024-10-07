aefcb11c3c0546dad2f0808be96bb1ac7524abc5
Gabrielhva <148148776+Gabrielhva@users.noreply.github.com>
2024-10-07 17:35:54 -0300

    assinatura pronta

--- pages/assinatura.vue
+++ pages/assinatura.vue
@@ -6,29 +6,31 @@ import '~/assets/css/assinatura.css'
 
 <template>
     
-<h1 class="tituloo">Atenda muito mais com Premium</h1>
+<h1 class="tituloo">Atenda muito mais com Premium <i class="fa-solid fa-bolt"></i></h1>
 
 <section class="fundoo">
 
 
 <div class="containerr">
 
-    <div class="imga">
-        <img src="./imagens/black.jpg"/>
+    
 
-        <img src="./imagens/black.jpg"/>
-    </div>
+        <img class="imga" src="/public/black.jpg"/>
+
+        <img class="imga" src="/public/black.jpg"/>
+        
+    
 
     <div class="texto-sobre-imagem">
 
-    <div class="textoo textoo2"><strong>Versão para Especialistas</strong></div>
+    <div class="textoo textoo2"><strong>Versão para Especialistas <i class="fa-solid fa-notes-medical"></i></strong></div>
     <div class="textoo textoo3"><strong>Doenças Mentais</strong></div>
     <div class="textoo espacoo"><strong>R$ 49,99 <p class="textoo4">/Mês</p></strong></div>
     <div class="textoo textoo5"><strong><span class="spann">✓</span> Destaque para os pacientes</strong></div>
     <div class="textoo textoo6"><strong><span class="spann">✓</span> Perfil mais personalizado</strong></div>
     <div class="textoo textoo7"><strong><span class="spann">✓</span> Maior taxa de resposta</strong></div>
 
-    <div class="textoo botaoo"><button>Assinar</button></div>
+    <div class="textoo botaoo"><button class="buttonn">Assinar</button></div>
 
 
     </div>
@@ -36,7 +38,7 @@ import '~/assets/css/assinatura.css'
     <div class="texto-sobre-imagem">
 
 
-    <div class="textoo textoo2"><strong>Versão para Clínicas</strong></div>
+    <div class="textoo textoo2"><strong>Versão para Clínicas <i class="fa-solid fa-house-medical"></i></strong></div>
     <div class="textoo textoo3"><strong>Dependência Química</strong></div>
     <div class="textoo espacoo"><strong>R$ 119,99 <p class="textoo4">/Mês</p></strong></div>
     <div class="textoo textoo5"><strong><span class="spann">✓</span> Destaque para os pacientes</strong></div>
